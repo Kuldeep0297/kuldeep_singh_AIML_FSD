afb76ec360990dcb022f32e42b23fdecfe3e9c84
Kuldeep0297 <kuldeep.23b1531144@abes.ac.in>
2025-11-06 21:16:44 -0700

    Update collapsible.jsx

--- collapsible.jsx
+++ collapsible.jsx
@@ -4,8 +4,10 @@ import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
 
 const Collapsible = CollapsiblePrimitive.Root
 
+
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
 const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
+
